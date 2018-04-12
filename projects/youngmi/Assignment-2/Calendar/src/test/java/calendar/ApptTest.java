70e201f336017123a325c4dd5763853289825690
youngmi <mwyoung2@gmail.com>
2018-04-12 15:59:46 -0700

    updated appt test formatiing

--- projects/youngmi/Assignment-2/Calendar/src/test/java/calendar/ApptTest.java
+++ projects/youngmi/Assignment-2/Calendar/src/test/java/calendar/ApptTest.java
@@ -2,26 +2,26 @@
 
 package calendar;
 
-import org.junit.Test;
+import org.junit.*;
 import static org.junit.Assert.*;
 import calendar.Appt;
 import calendar.CalendarUtil;
-public class ApptTest  {
 
-  @Test(timeout = 4000)
-  public void test00()  throws Throwable  {
-      Appt appt0 = new Appt(15, 30, 9, 14, 2018, "Birthday Party", "This is my birthday party", "xyz@gmail.com");
-      String string0 = appt0.toString();
-      assertEquals("Recur By", 2, appt0.getRecurBy());
-      assertFalse("Is Recurring", appt0.isRecurring());
-      assertEquals("Output string", "\t14/9/2018 at 3:30pm ,Birthday Party, This is my birthday party\n", string0);
-      assertEquals("Recur Increment", 0, appt0.getRecurIncrement());
-      appt0.setValid();
-  }
-  @Test(timeout = 4000)
-  public void test01()  throws Throwable  {
+public class ApptTest {
 
+	@Test(timeout = 4000)
+	public void test00() throws Throwable {
+		Appt appt0 = new Appt(15, 30, 9, 14, 2018, "Birthday Party", "This is my birthday party", "xyz@gmail.com");
+		String string0 = appt0.toString();
+		assertEquals("Recur By", 2, appt0.getRecurBy());
+		assertFalse("Is Recurring", appt0.isRecurring());
+		assertEquals("Output string", "\t14/9/2018 at 3:30pm ,Birthday Party, This is my birthday party\n", string0);
+		assertEquals("Recur Increment", 0, appt0.getRecurIncrement());
+		appt0.setValid();
+	}
 
-}
+	@Test(timeout = 4000)
+	public void test01() throws Throwable {
 
+	}
 }
