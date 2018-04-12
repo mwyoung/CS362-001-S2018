9d17c16261d89a92c48c7cbae4c0b250a80a81ac
youngmi <mwyoung2@gmail.com>
2018-04-12 14:53:58 -0700

    Changed bug in datahandler to allow for more code to run

--- projects/youngmi/Assignment-2/Calendar/src/test/java/calendar/ApptTest.java
+++ projects/youngmi/Assignment-2/Calendar/src/test/java/calendar/ApptTest.java
@@ -12,10 +12,10 @@ public class ApptTest  {
   public void test00()  throws Throwable  {
       Appt appt0 = new Appt(15, 30, 9, 14, 2018, "Birthday Party", "This is my birthday party", "xyz@gmail.com");
       String string0 = appt0.toString();
-      assertEquals(2, appt0.getRecurBy());
-      assertFalse(appt0.isRecurring());
-      assertEquals("\t14/9/2018 at 3:30pm ,Birthday Party, This is my birthday party\n", string0);
-      assertEquals(0, appt0.getRecurIncrement());
+      assertEquals("Recur By", 2, appt0.getRecurBy());
+      assertFalse("Is Recurring", appt0.isRecurring());
+      assertEquals("Output string", "\t14/9/2018 at 3:30pm ,Birthday Party, This is my birthday party\n", string0);
+      assertEquals("Recur Increment", 0, appt0.getRecurIncrement());
       appt0.setValid();
   }
 @Test(timeout = 4000)
