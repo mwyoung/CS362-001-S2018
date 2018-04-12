c569c53d677ea770470b34e072ece7be3d2823c6
youngmi <mwyoung2@gmail.com>
2018-04-12 16:09:45 -0700

    added appt test data enters

--- projects/youngmi/Assignment-2/Calendar/src/test/java/calendar/ApptTest.java
+++ projects/youngmi/Assignment-2/Calendar/src/test/java/calendar/ApptTest.java
@@ -22,6 +22,20 @@ public class ApptTest {
 
 	@Test(timeout = 4000)
 	public void test01() throws Throwable {
-
+		Appt appt0 = new Appt(15, 30, 9, 14, 2018, "Birthday Party", "This is my birthday party", "xyz@gmail.com");
+		assertEquals("Min", 15, appt0.getStartMinute());
+		assertEquals("Hour", 30, appt0.getStartHour());
+		assertEquals("Day", 9, appt0.getStartDay());
+		assertEquals("Month", 14, appt0.getStartMonth());
+		assertEquals("Year", 2018, appt0.getStartYear());
+		assertEquals("Title", "Birthday Party", appt0.getTitle());
+		assertEquals("Desc", "This is my birthday party", appt0.getDescription());
+		assertEquals("Email", "xyz@gmail.com", appt0.getEmailAddress());
+		assertTrue("Time", appt0.hasTimeSet());
+		assertTrue("True Occur", appt0.isOn(9, 14, 2018));
+		assertFalse("False Occur 1", appt0.isOn(10, 14, 2018));
+		assertFalse("False Occur 2", appt0.isOn(9, 15, 2018));
+		assertFalse("False Occur 3", appt0.isOn(9, 14, 2019));
 	}
+	
 }
