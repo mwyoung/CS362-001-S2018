a7fa6ecaf99c6786b59ba06cbf4722ef0925ced7
youngmi <mwyoung2@gmail.com>
2018-05-09 10:44:09 -0700

    appt setValid, isOn, setRecurDays 100% branch coverage

--- projects/youngmi/Assignment-4/Calendar/src/test/java/calendar/ApptRandomTest.java
+++ projects/youngmi/Assignment-4/Calendar/src/test/java/calendar/ApptRandomTest.java
@@ -10,8 +10,8 @@ import static org.junit.Assert.*;
  */
 
 public class ApptRandomTest {
-
-	private static final long TestTimeout = 60 * 500 * 1; /* Timeout at 30 seconds */
+	//private static final long TestTimeout = 60 * 500 * 1; /* Timeout at 30 seconds */
+	private static final long TestTimeout = 10*1000; //1000 = 1 second
 	private static final int NUM_TESTS=100;
 
 	/**
@@ -56,21 +56,22 @@ public class ApptRandomTest {
 
 		 long startTime = Calendar.getInstance().getTimeInMillis();
 		 long elapsed = Calendar.getInstance().getTimeInMillis() - startTime;
-
+		 int iteration = 0;
+		 long oldElapsed = 0;
 		 
 		 System.out.println("Start testing...");
 		 
-		try{ 
-			for (int iteration = 0; elapsed < TestTimeout; iteration++) {
+		 try{ 
+			for (iteration = 0; elapsed < TestTimeout; iteration++) {
 				long randomseed =System.currentTimeMillis(); //10
 				//System.out.println(" Seed:"+randomseed );
 				Random random = new Random(randomseed);
 				
-				 int startHour=ValuesGenerator.getRandomIntBetween(random, 1, 11);
-				 int startMinute=ValuesGenerator.getRandomIntBetween(random, 1, 11);
-				 int startDay=ValuesGenerator.getRandomIntBetween(random, 1, 11);
-				 int startMonth=ValuesGenerator.getRandomIntBetween(random, 1, 11);
-				 int startYear=ValuesGenerator.getRandomIntBetween(random, 2018, 2018);
+				 int startHour=ValuesGenerator.getRandomIntBetween(random, 0, 34) - 6;
+				 int startMinute=ValuesGenerator.getRandomIntBetween(random, 0, 64) - 1;
+				 int startDay=ValuesGenerator.getRandomIntBetween(random, 0, 36) -1;
+				 int startMonth=ValuesGenerator.getRandomIntBetween(random, 0, 14) -1;
+				 int startYear=ValuesGenerator.getRandomIntBetween(random, 1, 2025) - 5;
 				 String title="Birthday Party";
 				 String description="This is my birthday party.";
 				 String emailAddress="xyz@gmail.com";
@@ -78,34 +79,67 @@ public class ApptRandomTest {
 				 //Construct a new Appointment object with the initial data	 
 		         Appt appt = new Appt(startHour, startMinute, startDay, startMonth, 
 		        		 startYear, title, description, emailAddress);
-
-		         if(!appt.getValid())continue;
-		         for (int i = 0; i < NUM_TESTS; i++) {
-		        	 String methodName = ApptRandomTest.RandomSelectMethod(random);
-					   if (methodName.equals("setTitle")){
-						   String newTitle=(String) ValuesGenerator.getString(random);
-						   appt.setTitle(newTitle);						   
-						}
-					   else if (methodName.equals("setRecurrence")){
-						   int sizeArray=ValuesGenerator.getRandomIntBetween(random, 0, 8);
-						   int[] recurDays=ValuesGenerator.generateRandomArray(random, sizeArray);
-						   int recur=ApptRandomTest.RandomSelectRecur(random);
-						   int recurIncrement = ValuesGenerator.RandInt(random);
-						   int recurNumber = ApptRandomTest.RandomSelectRecurForEverNever(random);
-						   appt.setRecurrence(recurDays, recur, recurIncrement, recurNumber);
-						}				
+		       
+		         assertEquals(startHour,appt.getStartHour());
+		         assertEquals(startMinute,appt.getStartMinute());
+		         assertEquals(startDay,appt.getStartDay());
+		         assertEquals(startMonth,appt.getStartMonth());
+		         assertEquals(startYear,appt.getStartYear());
+		         
+		         //will not work otherwise- setValid fails in CalUtil NumDaysInMonth
+		         if(startMonth<12){	
+		        	 appt.setValid(); //set valid
 		         }
-				
+		         else if (startMonth>12 && startDay<28 && startDay>0 && startYear > 0){
+		           //System.out.println("Value: "+startDay+"."+startMonth+"."+startYear);
+		        	 appt.setValid(); //set valid
+		         }
+		         else { continue; } //not in this range, get new random numbers
+		         //if(!appt.getValid())continue; //check if not valid
+		         if(appt.getValid()){ //check if valid
+		        	 for (int i = 0; i < NUM_TESTS; i++) {
+		        		 String methodName = ApptRandomTest.RandomSelectMethod(random);
+		        		 if (methodName.equals("setTitle")){
+		        			 String newTitle=(String) ValuesGenerator.getString(random);
+		        			 appt.setTitle(newTitle);						   
+		        		 }
+		        		 else if (methodName.equals("setRecurrence")){
+		        			 int nullOrNot=ValuesGenerator.getRandomIntBetween(random, 0, 100);
+		        			 int[] recurDays;
+		        			 if(nullOrNot==0) {
+		        				 int sizeArray=ValuesGenerator.getRandomIntBetween(random, 0, 8);
+		        				 recurDays=ValuesGenerator.generateRandomArray(random, sizeArray);
+		        			 }
+		        			 else {
+		        				 //test setRecurdays
+		        				 recurDays = null;
+		        			 }
+		        			 
+		        			 int recur=ApptRandomTest.RandomSelectRecur(random);
+		        			 int recurIncrement = ValuesGenerator.RandInt(random);
+		        			 int recurNumber = ApptRandomTest.RandomSelectRecurForEverNever(random);
+		        			 appt.setRecurrence(recurDays, recur, recurIncrement, recurNumber);
+		        			 //test isOn
+		        			 assertTrue(appt.isOn(startDay, startMonth, startYear));
+		        			 assertFalse(appt.isOn(startDay-1, startMonth, startYear));
+		        			 assertFalse(appt.isOn(startDay, startMonth-1, startYear));
+		        			 assertFalse(appt.isOn(startDay, startMonth, startYear-1));
+		        		 }				
+		        	 }
+		         }
 				 elapsed = (Calendar.getInstance().getTimeInMillis() - startTime);
-			        if((iteration%10000)==0 && iteration!=0 )
-			              System.out.println("elapsed time: "+ elapsed + " of "+TestTimeout);
+				 //if((iteration%1000000)==0 && iteration!=0 ) { System.out.println("test: " + iteration + ", elapsed time: " + elapsed + " of "+TestTimeout); }
+				if(((elapsed%500)==0) && (iteration!=0) && (oldElapsed != elapsed)) { 
+					System.out.println("test: " + iteration + ", elapsed time: " + elapsed + " of "+TestTimeout); 
+					oldElapsed = elapsed;
+				}  
 			 
 			}
 		}catch(NullPointerException e){
 			
 		}
 	 
-		 System.out.println("Done testing...");
+		 System.out.println("Done testing, " + iteration + " tests.");
 	}
 
 }
