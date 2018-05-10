01e93f1b4c2f349bce0ba52faecb4e4ee3bf062c
youngmi <mwyoung2@gmail.com>
2018-05-09 19:59:48 -0700

    Working CalDay test on addAppt - 100% coverage

--- projects/youngmi/Assignment-4/Calendar/src/test/java/calendar/ApptRandomTest.java
+++ projects/youngmi/Assignment-4/Calendar/src/test/java/calendar/ApptRandomTest.java
@@ -69,72 +69,75 @@ public class ApptRandomTest {
 				//System.out.println(" Seed:"+randomseed );
 				Random random = new Random(randomseed);
 				
-				 int startHour=ValuesGenerator.getRandomIntBetween(random, 0, 34) - 6;
-				 int startMinute=ValuesGenerator.getRandomIntBetween(random, 0, 64) - 1;
-				 int startDay=ValuesGenerator.getRandomIntBetween(random, 0, 36) -1;
-				 int startMonth=ValuesGenerator.getRandomIntBetween(random, 0, 14) -1;
-				 int startYear=ValuesGenerator.getRandomIntBetween(random, 1, 2025) - 5;
-				 String title="Birthday Party";
-				 String description="This is my birthday party.";
-				 String emailAddress="xyz@gmail.com";
+				//random numbers
+				int startHour=ValuesGenerator.getRandomIntBetween(random, 0, 34) - 6;
+				int startMinute=ValuesGenerator.getRandomIntBetween(random, 0, 64) - 1;
+				int startDay=ValuesGenerator.getRandomIntBetween(random, 0, 36) -1;
+				int startMonth=ValuesGenerator.getRandomIntBetween(random, 0, 14) -1;
+				int startYear=ValuesGenerator.getRandomIntBetween(random, 1, 2025) - 5;
+				String title="Birthday Party";
+				String description="This is my birthday party.";
+				String emailAddress="xyz@gmail.com";
 
-				 //Construct a new Appointment object with the initial data	 
-		         Appt appt = new Appt(startHour, startMinute, startDay, startMonth, 
-		        		 startYear, title, description, emailAddress);
-		       
-		         assertEquals(startHour,appt.getStartHour());
-		         assertEquals(startMinute,appt.getStartMinute());
-		         assertEquals(startDay,appt.getStartDay());
-		         assertEquals(startMonth,appt.getStartMonth());
-		         assertEquals(startYear,appt.getStartYear());
-		         
-		         //will not work otherwise- setValid fails in CalUtil NumDaysInMonth
-		         if(startMonth<12){	
-		        	 appt.setValid(); //set valid
-		         }
-		         else if (startMonth>12 && startDay<28 && startDay>0 && startYear > 0){
-		           //System.out.println("Value: "+startDay+"."+startMonth+"."+startYear);
-		        	 appt.setValid(); //set valid
-		         }
-		         else { continue; } //not in this range, get new random numbers
-		         //if(!appt.getValid())continue; //check if not valid
-		         if(appt.getValid()){ //check if valid
-		        	 for (int i = 0; i < NUM_TESTS; i++) {
-		        		 String methodName = ApptRandomTest.RandomSelectMethod(random);
-		        		 if (methodName.equals("setTitle")){
-		        			 String newTitle=(String) ValuesGenerator.getString(random);
-		        			 appt.setTitle(newTitle);						   
-		        		 }
-		        		 else if (methodName.equals("setRecurrence")){
-		        			 int nullOrNot=ValuesGenerator.getRandomIntBetween(random, 0, 100);
-		        			 int[] recurDays;
-		        			 if(nullOrNot==0) {
-		        				 int sizeArray=ValuesGenerator.getRandomIntBetween(random, 0, 8);
-		        				 recurDays=ValuesGenerator.generateRandomArray(random, sizeArray);
-		        			 }
-		        			 else {
-		        				 recurDays = null;  //test setRecurdays
-		        			 }
-		        			 
-		        			 int recur=ApptRandomTest.RandomSelectRecur(random);
-		        			 int recurIncrement = ValuesGenerator.RandInt(random);
-		        			 int recurNumber = ApptRandomTest.RandomSelectRecurForEverNever(random);
-		        			 appt.setRecurrence(recurDays, recur, recurIncrement, recurNumber);
-		        			 //test isOn
-		        			 assertTrue(appt.isOn(startDay, startMonth, startYear));
-		        			 assertFalse(appt.isOn(startDay-1, startMonth, startYear));
-		        			 assertFalse(appt.isOn(startDay, startMonth-1, startYear));
-		        			 assertFalse(appt.isOn(startDay, startMonth, startYear-1));
-		        		 }				
-		        	 }
-		         }
-				 elapsed = (Calendar.getInstance().getTimeInMillis() - startTime);
-				 //if((iteration%1000000)==0 && iteration!=0 ) { System.out.println("test: " + iteration + ", elapsed time: " + elapsed + " of "+TestTimeout);}
+				//Construct a new Appointment object with the initial data	 
+				Appt appt = new Appt(startHour, startMinute, startDay, startMonth, 
+						startYear, title, description, emailAddress);
+
+				//check if equal
+				assertEquals(startHour,appt.getStartHour());
+				assertEquals(startMinute,appt.getStartMinute());
+				assertEquals(startDay,appt.getStartDay());
+				assertEquals(startMonth,appt.getStartMonth());
+				assertEquals(startYear,appt.getStartYear());
+
+				//will not work otherwise- setValid fails in CalUtil NumDaysInMonth
+				if(startMonth<12){	
+					appt.setValid(); //set valid
+				}
+				else if (startMonth>12 && startDay<28 && startDay>0 && startYear > 0){
+					//System.out.println("Value: "+startDay+"."+startMonth+"."+startYear);
+					appt.setValid(); //set valid
+				}
+				else { continue; } //not in this range, get new random numbers
+				//if(!appt.getValid())continue; //check if not valid
+				if(appt.getValid()){ //check if valid
+					for (int i = 0; i < NUM_TESTS; i++) {
+						//get random method to test
+						String methodName = ApptRandomTest.RandomSelectMethod(random);
+						if (methodName.equals("setTitle")){ //title test
+							String newTitle=(String) ValuesGenerator.getString(random);
+							appt.setTitle(newTitle);						   
+						}
+						else if (methodName.equals("setRecurrence")){ //recurrence test
+							int nullOrNot=ValuesGenerator.getRandomIntBetween(random, 0, 30);
+							int[] recurDays;
+							if(nullOrNot!=0) { 
+								int sizeArray=ValuesGenerator.getRandomIntBetween(random, 0, 8);
+								recurDays=ValuesGenerator.generateRandomArray(random, sizeArray);
+							}
+							else { //if 0 -> then use a null value
+								recurDays = null;  //test setRecurdays
+							}
+							//setup recurrence
+							int recur=ApptRandomTest.RandomSelectRecur(random);
+							int recurIncrement = ValuesGenerator.RandInt(random);
+							int recurNumber = ApptRandomTest.RandomSelectRecurForEverNever(random);
+							appt.setRecurrence(recurDays, recur, recurIncrement, recurNumber);
+							//test isOn - each number
+							assertTrue(appt.isOn(startDay, startMonth, startYear));
+							assertFalse(appt.isOn(startDay-1, startMonth, startYear));
+							assertFalse(appt.isOn(startDay, startMonth-1, startYear));
+							assertFalse(appt.isOn(startDay, startMonth, startYear-1));
+						}				
+					}
+				}
+				elapsed = (Calendar.getInstance().getTimeInMillis() - startTime);
+				//if((iteration%1000000)==0 && iteration!=0 ) { System.out.println("test: " + iteration + ", elapsed time: " + elapsed + " of "+TestTimeout);}
 				if(((elapsed%500)==0) && (iteration!=0) && (oldElapsed != elapsed)) { 
 					System.out.println("test: " + iteration + ", elapsed time: " + elapsed + " of "+TestTimeout); 
 					oldElapsed = elapsed;
 				}  
-			 
+
 			}
 		}catch(NullPointerException e){
 			
