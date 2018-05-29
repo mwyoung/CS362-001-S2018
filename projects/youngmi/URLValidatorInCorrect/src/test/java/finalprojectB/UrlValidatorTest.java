4221be197f97bd1409dd135edb1c56f4b391fa05
youngmi <mwyoung2@gmail.com>
2018-05-29 09:47:38 -0700

    New manual loops

--- projects/youngmi/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
+++ projects/youngmi/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
@@ -18,6 +18,9 @@ public class UrlValidatorTest extends TestCase {
 		assertFalse(firstOutput);	//does not work?
 		System.out.println("Outputs: " + firstOutput);
 		
+		//bad url
+		assertFalse(urlValue.isValid(""));
+		
 		System.out.println("!!(valid/invalid) means output is bad");
 		String[] falseURLs = {"http://oregonstate.edu","qwerty"};
 		for(int i=0;i<falseURLs.length;i++) {
@@ -38,29 +41,25 @@ public class UrlValidatorTest extends TestCase {
 				System.out.println("!!invalid: " + trueURLs[i]);
 			}
 		}
-		/*
-		//IP addresses
-		assertTrue(urlValue.isValid("http://example.org")); 
-		assertTrue(urlValue.isValid("http://93.184.215.34"));
-		//assertTrue(urlValue.isValid("https://93.184.215.34"));
+		String[] trueIPs = {"http://93.184.215.34","https://93.184.215.34","ftp://93.184.215.34",
+				"http://93.184.216.34/","http://93.184.216.34:80","http://93.184.215.34/example/",
+				"ftp://93.184.215.34/~example/"};
+		for(int i=0;i<trueIPs.length;i++) {
+			if(urlValue.isValid(trueIPs[i])){
+				System.out.println("    valid: " + trueIPs[i]);
+			} 
+			else {
+				System.out.println("!!invalid: " + trueIPs[i]);
+			}
+		}
 
-		assertFalse(urlValue.isValid("ftp://93.184.215.34"));	//maybe?
-		assertTrue(urlValue.isValid("http://93.184.216.34/"));	//example.org
-		assertFalse(urlValue.isValid("http://93.184.216.34:80")); //!!Fails!!
-		assertFalse(urlValue.isValid("http://93.184.215.34/example/")); //!!fails
-		
-		//assertTrue(urlValue.isValid("ftp://93.184.215.34/~example/"));
-		
-		//true URLs
-		assertTrue(urlValue.isValid("ftp://example.com"));
-
-		//False URLs
-		assertFalse(urlValue.isValid(""));
-		assertFalse(urlValue.isValid("example.org"));
-*/
 		//try{}catch(Exception e){}
 		//if(urlValue.isValid("http://oregonstate.com")){System.out.println("valid");} 
 		//else { System.out.println("invalid");}	//testing
+	}
+	public void testManualTest_crash() {
+		UrlValidator urlValue = new UrlValidator(null);
+		assertTrue(urlValue.isValid("http://www.google.com")); //does not work
 	}
 	
 	//First Partition testing	
